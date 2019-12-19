b03c4bf02bcbd3b30bc3e19c92d6a08e44e78bd1
uki00a <uki00a@gmail.com>
2019-12-20 06:03:00 +0900

    refactor(test): move tests in mod_test.ts to tests/key_methods_test.ts

--- mod_test.ts
+++ mod_test.ts
@@ -1,47 +1,9 @@
-import { runTests, test } from './vendor/https/deno.land/std/testing/mod.ts';
-import { assertStrictEq } from './vendor/https/deno.land/std/testing/asserts.ts';
-import { createMockRedis } from './mod.ts';
+import { runTests } from './vendor/https/deno.land/std/testing/mod.ts';
 
+import './tests/keys_methods_test.ts';
 import './tests/string_methods_test.ts';
 import './tests/list_methods_test.ts';
 import './tests/set_methods_test.ts';
 import './tests/hash_methods_test.ts';
-
-test(async function exists() {
-  const redis = createMockRedis();
-  // TODO replace with `SET`
-  await redis.lpush('key1', 'Hello');
-
-  assertStrictEq(await redis.exists('key1'), 1);
-  assertStrictEq(await redis.exists('nosuchkey'), 0);
-
-  await redis.lpush('key2', 'World');
-
-  assertStrictEq(await redis.exists('key1', 'key2', 'nosuchkey'), 2);
-});
-
-test(async function del() {
-  {
-    const redis = createMockRedis();
-    assertStrictEq(await redis.del('nosuchkey'), 0);
-  }
-
-  {
-    const redis = createMockRedis();
-    await redis.rpush('mylist', 'one');
-    await redis.sadd('myset', 'one');
-    assertStrictEq(await redis.del('mylist', 'nosuchkey'), 1);
-    assertStrictEq(await redis.exists('mylist'), 0, 'should remove specified keys');
-  }
-
-  {
-    const redis = createMockRedis();
-    await redis.rpush('mylist', 'one');
-    await redis.sadd('myset', 'one');
-    assertStrictEq(await redis.del('mylist', 'mylist', 'myset', 'nosuchkey', 'mystring'), 2);
-    assertStrictEq(await redis.exists('mylist'), 0, 'should remove specified keys');
-    assertStrictEq(await redis.exists('myset'), 0, 'should remove specified keys');
-  }
-});
 
 runTests();
---
+++ tests/keys_methods_test.ts
@@ -0,0 +1,42 @@
+import { runIfMain, test } from '../vendor/https/deno.land/std/testing/mod.ts';
+import { assertStrictEq } from '../vendor/https/deno.land/std/testing/asserts.ts';
+import { createMockRedis } from '../mod.ts';
+
+test(async function exists() {
+  const redis = createMockRedis();
+  // TODO replace with `SET`
+  await redis.lpush('key1', 'Hello');
+
+  assertStrictEq(await redis.exists('key1'), 1);
+  assertStrictEq(await redis.exists('nosuchkey'), 0);
+
+  await redis.lpush('key2', 'World');
+
+  assertStrictEq(await redis.exists('key1', 'key2', 'nosuchkey'), 2);
+});
+
+test(async function del() {
+  {
+    const redis = createMockRedis();
+    assertStrictEq(await redis.del('nosuchkey'), 0);
+  }
+
+  {
+    const redis = createMockRedis();
+    await redis.rpush('mylist', 'one');
+    await redis.sadd('myset', 'one');
+    assertStrictEq(await redis.del('mylist', 'nosuchkey'), 1);
+    assertStrictEq(await redis.exists('mylist'), 0, 'should remove specified keys');
+  }
+
+  {
+    const redis = createMockRedis();
+    await redis.rpush('mylist', 'one');
+    await redis.sadd('myset', 'one');
+    assertStrictEq(await redis.del('mylist', 'mylist', 'myset', 'nosuchkey', 'mystring'), 2);
+    assertStrictEq(await redis.exists('mylist'), 0, 'should remove specified keys');
+    assertStrictEq(await redis.exists('myset'), 0, 'should remove specified keys');
+  }
+});
+
+runIfMain(import.meta);
