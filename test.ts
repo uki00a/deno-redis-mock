eb4478d07f691fe5325d43cd03e2c95d07949566
uki00a <uki00a@gmail.com>
2019-12-24 05:47:25 +0900

    test(zset): add import to test.ts

--- test.ts
+++ test.ts
@@ -5,5 +5,6 @@ import './tests/string_methods_test.ts';
 import './tests/list_methods_test.ts';
 import './tests/set_methods_test.ts';
 import './tests/hash_methods_test.ts';
+import './tests/zset_methods_test.ts';
 
 runTests();
